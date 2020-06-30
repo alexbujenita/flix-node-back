de0b918128ac5722f3cc239df2b316efa80c2262
alexbujenita <dekc89@gmail.com>
2020-06-30 21:17:09 +0100

    better folder structure using sequelize-cli

--- index.js
+++ index.js
@@ -1,91 +1,14 @@
 const express = require("express");
 const applyApi = require("./src/routes").applyApi;
-const { Sequelize, DataTypes, Model } = require("sequelize");
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
-});
-
-// class User extends Model {}
-// class UserFavourite extends Model {}
-
-// User.init(
-//   {
-//     // Model attributes are defined here
-//     firstName: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     lastName: {
-//       type: DataTypes.STRING,
-//       // allowNull defaults to true
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true,
-//     },
-//     passwordDigest: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//   },
-//   {
-//     // Other model options go here
-//     sequelize, // We need to pass the connection instance
-//     modelName: "User", // We need to choose the model name
-//   }
-// );
-
-// UserFavourite.init(
-//   {
-//     movieRefId: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     movieTitle: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     moviePosterPath: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     seen: {
-//       type: DataTypes.BOOLEAN,
-//       defaultValue: false,
-//     },
-//     description: {
-//       type: DataTypes.TEXT,
-//     },
-//   },
-//   {
-//     // Other model options go here
-//     sequelize, // We need to pass the connection instance
-//     modelName: "UserFavourite", // We need to choose the model name
-//   }
-// );
-
-// User.hasMany(UserFavourite);
 
 const app = express();
+
 applyApi(app);
 
 app.get("/", (req, resp) => {
   resp.send("test");
 });
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-  // await sequelize.sync({ force: true });
-  app.listen(3001, () => {
-    console.log("Server started on port 3001");
-  });
-})();
-
-// exports.models = { User, UserFavourite };
+app.listen(3001, () => {
+  console.log("Server started on port 3001");
+});
--- src/routes/movie/index.js
+++ src/routes/movie/index.js
@@ -2,18 +2,10 @@ const movieRouter = require("express").Router();
 const axios = require("axios");
 const API_KEY = require("../../../secrets").API_KEY;
 const { colours } = require("../../utils/colours");
-const db = require("../../../models")
 
 const movieCache = new Map();
 
 movieRouter.get("/:movieId", async (req, res) => {
-  console.log(db.User)
-  // const user = await db.User.create({firstName: 'AAA', email: 'asd', passwordDigest: 'asdasda'})
-  // const asd = await db.UserFavourite.create({movieRefId: 2243,movieTitle: "adasd",userId: 1,moviePosterPath:"asdada"})
-  await db.UserFavourite.create({movieRefId: 22343,movieTitle: "aASDASDdasd",userId: 1,moviePosterPath:"aDGARREFEEFsdada"})
-  const usQ = await db.User.findByPk(1)
-  const usFav = await usQ.getUserFavourites()
-  console.log(usFav)
   const {
     params: { movieId },
   } = req;
