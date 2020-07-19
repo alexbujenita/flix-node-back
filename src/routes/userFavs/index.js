95187b6768a466e33840f0c50c0684ef4bbd32e1
alexbujenita <dekc89@gmail.com>
2020-07-19 17:00:50 +0100

    PATCH route for favs

--- src/routes/userFavs/index.js
+++ src/routes/userFavs/index.js
@@ -3,38 +3,47 @@ const db = require("../../../models/index");
 
 userFavsRouter.post("/", async (req, res) => {
   const {
-    body: {
-      movieRefId,
-      movieTitle,
-      moviePosterPath,
-      seen,
-      description,
-      userId,
-    },
+    body: { movieRefId, movieTitle, moviePosterPath, userId },
   } = req;
   try {
     const fav = await db.UserFavourite.create({
       movieRefId: parseInt(movieRefId),
       movieTitle,
       moviePosterPath,
-      seen: Boolean(seen),
-      description,
-      userId: parseInt(userId)
-    })
+      userId: parseInt(userId),
+    });
     res.send(fav);
   } catch (e) {
     res.status(500).send(e);
   }
 });
 
-userFavsRouter.delete('/:favId', async (req, res) => {
-  const { params: { favId } } = req;
+userFavsRouter.patch("/:favId", async (req, res) => {
+  const {
+    params: { favId },
+    body: { seen, description }
+  } = req;
+  try {
+    const fav = await db.UserFavourite.findByPk(parseInt(favId));
+    if(seen) fav.seen = !fav.seen;
+    if(description) fav.description = description;
+    await fav.save();
+    res.send(fav);
+  } catch {
+    res.status(500).send({ error: "Resource not found." });
+  }
+});
+
+userFavsRouter.delete("/:favId", async (req, res) => {
+  const {
+    params: { favId },
+  } = req;
   try {
     const fav = await db.UserFavourite.findByPk(parseInt(favId));
     await fav.destroy();
     res.status(204).send();
   } catch {
-    res.status(500).send({ error: 'Resource not found.' });
+    res.status(500).send({ error: "Resource not found." });
   }
 });
 
