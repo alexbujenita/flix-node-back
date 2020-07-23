0f63b5379b10576cc3c47c0de97a0650f7b670bd
alexbujenita <dekc89@gmail.com>
2020-07-23 22:10:05 +0100

    GET to retrieve all favs of one user, needs some attention

--- src/routes/userFavs/index.js
+++ src/routes/userFavs/index.js
@@ -1,6 +1,27 @@
 const userFavsRouter = require("express").Router();
 const db = require("../../../models/index");
 
+userFavsRouter.get("/:userId", async (req, res) => {
+  const {
+    params: { userId },
+  } = req;
+  try {
+    const userFavs = await db.User.findByPk(parseInt(userId), {
+      include: db.UserFavourite,
+    });
+    // console.log(email);
+    // const favs = await db.User.findOne({
+    //   where: { email },
+    //   include: db.UserFavourite,
+    // });
+    if (!userFavs) throw new Error("User not found.");
+    res.send(userFavs);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
 userFavsRouter.post("/", async (req, res) => {
   const {
     body: { movieRefId, movieTitle, moviePosterPath, userId },
@@ -21,12 +42,12 @@ userFavsRouter.post("/", async (req, res) => {
 userFavsRouter.patch("/:favId", async (req, res) => {
   const {
     params: { favId },
-    body: { seen, description }
+    body: { seen, description },
   } = req;
   try {
     const fav = await db.UserFavourite.findByPk(parseInt(favId));
-    if(seen) fav.seen = !fav.seen;
-    if(description) fav.description = description;
+    if (seen) fav.seen = !fav.seen;
+    if (description) fav.description = description;
     await fav.save();
     res.send(fav);
   } catch {
