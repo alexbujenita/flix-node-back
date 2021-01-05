3e3e6f4a7c9e3639c9e10e2e68ca5b7ea0dc997e
alexbujenita <dekc89@gmail.com>
2021-01-05 16:49:50 +0000

    added caching around search route

--- src/routes/search/index.js
+++ src/routes/search/index.js
@@ -1,18 +1,29 @@
 const searchRouter = require("express").Router();
 const axios = require("axios");
 const API_KEY = require("../../../secrets").API_KEY;
+const { colours } = require("../../utils/colours");
+
+const searchCache = new Map();
 
 searchRouter.get("/", async (req, res) => {
   const {
     query: { searchTerm, pageNum = 1, includeAdult = false },
   } = req;
-  try {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm.trim()}&page=${pageNum}&include_adult=${includeAdult}`
-    );
-    res.send(data);
-  } catch {
-    res.status(501).send("Internal server error");
+  const qString = searchTerm.trim() + pageNum;
+
+  if (searchCache.has(qString)) {
+    console.log(colours.FgCyan, "Search Movies Cache HIT!!!");
+    res.send(searchCache.get(qString));
+  } else {
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm.trim()}&page=${pageNum}&include_adult=${includeAdult}`
+      );
+      searchCache.set(qString, data);
+      res.send(data);
+    } catch {
+      res.status(501).send("Internal server error");
+    }
   }
 });
 
