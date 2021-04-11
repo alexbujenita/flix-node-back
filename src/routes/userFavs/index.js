c1de79081a27d2982ed054650ec629b3edf0e9fa
alexbujenita <dekc89@gmail.com>
2021-04-11 23:02:00 +0100

    PDF with title, tagline, poster image, overview, duration time and cast

--- src/routes/userFavs/index.js
+++ src/routes/userFavs/index.js
@@ -40,31 +40,92 @@ userFavsRouter.get("/pdf", authJWT, async (req, res) => {
           overview,
           original_title,
           poster_path,
+          credits: { cast },
         },
       } = await axios.get(
-        `https://api.themoviedb.org/3/movie/${movieRefId}?api_key=${API_KEY}`
+        `https://api.themoviedb.org/3/movie/${movieRefId}?api_key=${API_KEY}&append_to_response=credits`
       );
       doc
         .font("Helvetica")
         .fontSize(25)
         .text(title || original_title);
-      tagline && doc.font("Helvetica").fontSize(18).text(tagline);
+      tagline && doc.font("Helvetica").fontSize(16).text(tagline);
 
       if (poster_path) {
-        const {
-          data,
-        } = await axios(`https://image.tmdb.org/t/p/w342${poster_path}`, {
-          responseType: "arraybuffer",
-        });
+        const { data } = await axios(
+          `https://image.tmdb.org/t/p/w342${poster_path}`,
+          {
+            responseType: "arraybuffer",
+          }
+        );
         const img = Buffer.from(data, "base64");
-        doc.image(img);
+        doc.image(img, {scale: 0.75 });
       }
 
-      overview && doc.font("Helvetica").fontSize(14).text(overview);
+      doc.font("Helvetica").fontSize(14).text("\n", { lineGap: 5 });
+      overview &&
+        doc.font("Helvetica").fontSize(14).text(overview, { lineGap: 5 });
+      runtime &&
+        doc
+          .font("Helvetica-Bold")
+          .fontSize(16)
+          .text("Runtime: " + runtime + " minutes.", { lineGap: 5 });
+
+      if (cast && cast.length) {
+        doc.addPage();
+        doc.font("Helvetica").fontSize(18).text("Starring:", {align: "center"});
+        doc.font("Helvetica").fontSize(14).text('\n');
+
+        let height = 100;
+        for (let i = 0; i < 10 && i < cast.length; i++) {
+          if (cast[i].profile_path) {
+            const { data } = await axios(
+              `https://image.tmdb.org/t/p/w185${cast[i].profile_path}`,
+              {
+                responseType: "arraybuffer",
+              }
+            );
+            const img = Buffer.from(data, "base64");
+            
+            doc
+              .image(img, 50, height, { align: "center", scale: 0.5 }).fontSize(14)
+              .text(cast[i].name + " as " + cast[i].character, 50, height + 50, {align: "center"});
+            
+            height += 150;
+
+            if(i === 4) {
+              doc.addPage()
+              height = 50;
+            }
+
+          } else {
+            doc
+              .font("Helvetica")
+              .fontSize(14)
+              .text(cast[i].name + " as " + cast[i].character, 50, height + 50, {align: "center"});
+
+            height += 150;
+          }
+        }
+      }
+
       doc.addPage();
+      // break;
     }
 
+    doc
+      .font("Helvetica")
+      .fontSize(25)
+      .text("Some text with an embedded font!", { margin: 10 });
+    doc
+      .font("Helvetica")
+      .fontSize(25)
+      .text("Some text with an embedded font!", { lineGap: 10 });
     doc.font("Helvetica").fontSize(25).text("Some text with an embedded font!");
+    doc
+      .font("Helvetica")
+      .fontSize(25)
+      .text("Some text with an embedded font!", { margin: 10 });
 
     doc
       .addPage()
